8376a37300914f5373e3c7e5e9f65236307a60e1
TheDarkestNightRises <icecold363@gmail.com>
2023-04-13 00:39:57 +0200

    Made destroy.sh for car-service

--- K8s/deploy.sh
+++ K8s/deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-#Deployments
+#Car Service
 kubectl apply -f cars-depl.yaml
-
-#Database
 kubectl apply -f car-local-pvc.yaml
 kubectl apply -f cars-mssql-depl.yaml
 
-# Dont forget to kubectl create secret generic car-mssql --from-literal=SA_PASSWORD="pa55w0rd"
+# Dont forget to kubectl create secret generic car-mssql --from-literal=SA_PASSWORD="Pa55w0rd!"
 
 #Ingress nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
--- K8s/destroy-deploy.sh
+++ K8s/destroy-deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#------------Car Service--------------
+
+# Deployments
 kubectl delete deployment cars-depl
+kubectl delete deployment cars-mssql-depl
+
+# Services
+kubectl delete service car-mssql-clusterip-srv
+kubectl delete service cars-clusterip-srv
+kubectl delete service cars-mssql-clusterip-srv
+kubectl delete service cars-mssql-loadbalancer
+
 #Ingress loadbalancer
 kubectl delete deployment ingress-nginx-controller -n ingress-nginx
