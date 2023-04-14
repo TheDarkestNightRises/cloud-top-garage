b3553e61efe351e0eea3f936cb7c4994bd1e4b5c
Oriana11 <rubyjpy@gmail.com>
2023-04-14 12:46:02 +0200

    Create UsersController.cs

---
+++ UserService/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Dtos;
+using UserService.Logic;
+
+namespace UserService.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserLogic _userLogic;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserLogic userLogic, IMapper mapper)
+    {
+        _userLogic = userLogic;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserReadDto>>> GetAllUsers()
+    {
+        var users = await _userLogic.GetAllUsersAsync();
+        var usersMapped = _mapper.Map<IEnumerable<UserReadDto>>(users);
+        return Ok(usersMapped);
+    }
+}
