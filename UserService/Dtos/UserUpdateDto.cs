b5a9be556e7f0ab2f0119f937270ffbc283bed7b
Lichiag21 <lichiag21@gmail.com>
2023-04-19 00:02:49 +0200

    updated user update dto

--- UserService/Dtos/UserUpdateDto.cs
+++ UserService/Dtos/UserUpdateDto.cs
@@ -1,8 +1,6 @@
 namespace UserService.Dtos;
 public class UserUpdateDto
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
     public string Email { get; set; }
     public string Password {get; set;}
 }
