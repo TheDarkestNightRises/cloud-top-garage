ac46b29df5e3cc47e27fe25810ee2525dc006668
Lichiag21 <lichiag21@gmail.com>
2023-04-19 01:13:16 +0200

    users migration and user read dto updated

--- UserService/Data/SeedDb.cs
+++ UserService/Data/SeedDb.cs
@@ -37,8 +37,8 @@ public static class PrepDb
             Console.WriteLine("--> Seeding Data...");
 
             context.Users.AddRange(
-                new User { Name = "John Doe", Email = "johndoe@email.com" },
-                new User { Name = "Jane Smith", Email = "janesmith@email.com" }
+                new User { Name = "John Doe", Email = "johndoe@email.com", Password = "123" },
+                new User { Name = "Jane Smith", Email = "janesmith@email.com", Password = "1234" }
             );
 
             context.SaveChanges();
--- UserService/Dtos/UserReadDto.cs
+++ UserService/Dtos/UserReadDto.cs
@@ -4,6 +4,7 @@ public class UserReadDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public string Password {get; set;}
 
 }
 
