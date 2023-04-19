a5fb0e8a32e946e6f31479c048ed6b30fc558aae
Oriana11 <rubyjpy@gmail.com>
2023-04-19 11:59:27 +0200

    Made interface definition

--- CarService/Application/LogicContracts/ICarLogic.cs
+++ CarService/Application/LogicContracts/ICarLogic.cs
@@ -8,4 +8,6 @@ public interface ICarLogic
     public Task<IEnumerable<Car>> GetAllCarsAsync();
 
     public Task<IEnumerable<Car>> GetAllCarsAsync(CarQuery carQuery);
+
+    public Task<IEnumerable<Car>> DeleteCarAsync(int Id);
 }
