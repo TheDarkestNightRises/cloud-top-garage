ebafa7aff1769af22992f6a501766e40b7aee38e
Oriana11 <rubyjpy@gmail.com>
2023-04-19 14:29:21 +0200

    ICarLogic delete refactoring

--- CarService/Application/LogicContracts/ICarLogic.cs
+++ CarService/Application/LogicContracts/ICarLogic.cs
@@ -9,5 +9,5 @@ public interface ICarLogic
     public Task<IEnumerable<Car>> GetAllCarsAsync();
     public Task<IEnumerable<Car>> GetAllCarsAsync(CarQuery carQuery);
     public Task<Car> GetCarAsync(int id);
-    public Task<IEnumerable<Car>> DeleteCarAsync(int Id);
+    public Task DeleteCarAsync(int Id);
 }
