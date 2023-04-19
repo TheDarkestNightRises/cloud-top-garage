e031d221aa241931c04ad7c75ae8a5a8f8463c96
Oriana11 <rubyjpy@gmail.com>
2023-04-19 12:37:10 +0200

    Made Delete Car Database

--- CarService/Data/CarRepository.cs
+++ CarService/Data/CarRepository.cs
@@ -17,9 +17,15 @@ public class CarRepository : ICarRepository
         throw new NotImplementedException();
     }
 
-    public Task<Car> DeleteCarAsync(int id)
+    public async Task<Car> DeleteCarAsync(int id)
     {
-        throw new NotImplementedException();
+        var carToDelete = await  _context.Cars.FindAsync(id);
+        if (carToDelete != null)
+        {
+            _context.Cars.Remove(carToDelete);
+            await _context.SaveChangesAsync();
+        }
+        return carToDelete;
     }
 
     public async Task<IEnumerable<Car>> GetAllCarsAsync()
