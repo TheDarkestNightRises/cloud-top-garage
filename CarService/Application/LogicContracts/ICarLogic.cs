7bc41bec47cae875c7e08d4ef8fcc403a02fef38
Oriana11 <rubyjpy@gmail.com>
2023-04-19 13:38:37 +0200

    Added get car async method in ICarLogic.cs

--- CarService/Application/LogicContracts/ICarLogic.cs
+++ CarService/Application/LogicContracts/ICarLogic.cs
@@ -8,4 +8,5 @@ public interface ICarLogic
     public Task<Car> CreateAsync(Car car);
     public Task<IEnumerable<Car>> GetAllCarsAsync();
     public Task<IEnumerable<Car>> GetAllCarsAsync(CarQuery carQuery);
+    public Task<Car> GetCarAsync(int id);
 }
