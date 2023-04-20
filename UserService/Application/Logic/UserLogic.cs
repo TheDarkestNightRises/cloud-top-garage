45df524e178c54fe669a8a543fea35e2aba8bd62
Lichiag21 <lichiag21@gmail.com>
2023-04-21 00:33:44 +0200

    auth logic implemented

--- UserService/Application/Logic/UserLogic.cs
+++ UserService/Application/Logic/UserLogic.cs
@@ -18,17 +18,28 @@ public class UserLogic : IUserLogic
         return await _userRepository.GetAllUsersAsync();
     }
 
-    public Task<User> LoginUserAsync(string email, string password)
+    public async Task<User> LoginUserAsync(string email, string password)
     {
-        throw new NotImplementedException();
+        User? userFound = await _userRepository.getUserByEmail(email);
+
+        if (userFound == null)
+        {
+            throw new Exception("Email doesn't exist!");
+        }
+        if (userFound.Password.Equals(password))
+        {
+            throw new Exception("Wrong password!");
+        }
+        return userFound;
+
     }
 
     public async Task<User> UpdateUser(User userToUpdate)
     {
         User? userFound = await _userRepository.GetUserByIdAsync(userToUpdate.Id);
-        if(userFound == null) 
+        if (userFound == null)
         {
-           throw new Exception($"There is no user with the id: {userToUpdate.Id}");
+            throw new Exception($"There is no user with the id: {userToUpdate.Id}");
         }
         userFound.Email = userToUpdate.Email;
         userFound.Password = userToUpdate.Password;
--- UserService/Data/IUserRepository.cs
+++ UserService/Data/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace UserService.Data;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User?> getUserByEmail(string email);
     Task<User?> GetUserByIdAsync(int id);
     Task<User> UpdateUserAsync(User userFound);
 }
