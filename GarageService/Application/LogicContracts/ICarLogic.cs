bf18bec26c3d2cb48466241bba3fddc88364ae6e
Oriana11 <rubyjpy@gmail.com>
2023-04-21 11:06:38 +0200

    Create ICarLogic.cs

---
+++ GarageService/Application/LogicContracts/ICarLogic.cs
@@ -0,0 +1,10 @@
+using GarageService.Models;
+
+namespace GarageService.Application.LogicContracts;
+
+public interface ICarLogic
+{
+    Task DeleteCarAsync(int id);
+    Task<Car> GetCarByIdAsync(int carId);
+
+}
