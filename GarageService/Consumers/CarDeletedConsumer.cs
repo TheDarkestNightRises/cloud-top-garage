005ee1ee3c7ab8fd8ca96f125c7588fe7734b3bd
Oriana11 <rubyjpy@gmail.com>
2023-04-21 10:56:14 +0200

    Made dependecy injection for CarDeletedConsumer

--- GarageService/Consumers/CarDeletedConsumer.cs
+++ GarageService/Consumers/CarDeletedConsumer.cs
@@ -1,10 +1,17 @@
 using Contracts;
+using GarageService.Application.LogicContracts;
 using MassTransit;
 
 namespace GarageService.Consumers;
 
 public class CarDeletedConsumer : IConsumer<CarDeleted>
 {
+    private readonly IGarageLogic _garageLogic;
+
+    public CarDeletedConsumer(IGarageLogic garageLogic)
+    {
+        _garageLogic = garageLogic;
+    }
     public Task Consume(ConsumeContext<CarDeleted> context)
     {
         throw new NotImplementedException();
