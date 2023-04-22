9f13ddbdec632eec251edb7202deaad27946763b
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-22 14:16:04 +0200

    Get garage by id method for repository

--- GarageService/Data/IGarageRespository.cs
+++ GarageService/Data/IGarageRespository.cs
@@ -6,5 +6,5 @@ public interface IGarageRepository
 {
     Task<IEnumerable<Garage>> GetAllGaragesAsync();
     Task<IEnumerable<Garage>> GetAllGaragesAsync(GarageQuery garageQuery);
-
+    Task<Garage> GetGarageAsync(int id);
 }
