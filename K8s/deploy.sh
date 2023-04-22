5f0a7fb99886e70ae336fc63bd83db093b1276c0
TheDarkestNightRises <icecold363@gmail.com>
2023-04-22 09:20:07 +0200

    Refactored deploy.sh

--- K8s/deploy.sh
+++ K8s/deploy.sh
@@ -3,39 +3,30 @@
 #RabbitMq
 kubectl apply -f rabbitmq-depl.yaml 
 
-#Car Service
-kubectl apply -f car-mssql.yaml
-kubectl apply -f car-local-pvc.yaml
-kubectl apply -f car-mssql-initdb.yaml
-kubectl apply -f cars-mssql-depl.yaml
-kubectl apply -f cars-depl.yaml
+#!/bin/bash
 
-#User service
-kubectl apply -f user-local-pvc.yaml 
-kubectl apply -f user-mssql-initdb.yaml
-kubectl apply -f users-mssql-depl.yaml
-kubectl apply -f users-depl.yaml
+# Deploy CarService
+cd CarService
+./deploy-car.sh
+cd ..
 
-#Environment service
-kubectl apply -f environment-local-pvc.yaml 
-kubectl apply -f environment-mssql-initdb.yaml
-kubectl apply -f environments-mssql-depl.yaml
-kubectl apply -f environments-depl.yaml
+# Deploy Garage
+cd Garage
+./deploy-garage.sh
+cd ..
 
-#Garage Service
-kubectl apply -f garage-local-pvc.yaml
-kubectl apply -f garage-mssql-initdb.yaml
-kubectl apply -f garages-mssql-depl.yaml
-kubectl apply -f garages-depl.yaml
+# Deploy User
+cd User
+./deploy-user.sh
+cd ..
 
+# Deploy Environment
+cd Environment
+./deploy-environment.sh
+cd ..
 
-# Dont forget to kubectl create secret generic car-mssql --from-literal=SA_PASSWORD="Pa55w0rd!"
 
 #Ingress nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f ingress-srv.yaml
 
-# # Ocelot gateway
-# kubectl apply -f ocelot-config.yaml
-# kubectl apply -f ocelot-depl.yaml
-# kubectl apply -f ocelot-ingress.yaml
