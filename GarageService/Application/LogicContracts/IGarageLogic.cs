80596f357a8b4492f650e340920a67790be221ca
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-22 14:32:28 +0200

    get garage logic

--- GarageService/Application/LogicContracts/IGarageLogic.cs
+++ GarageService/Application/LogicContracts/IGarageLogic.cs
@@ -6,4 +6,5 @@ public interface IGarageLogic
 {
     public Task<IEnumerable<Garage>> GetAllGaragesAsync();
     public Task<IEnumerable<Garage>> GetAllGaragesAsync(GarageQuery garageQuery);
+    public Task<Garage> GetGarageAsync(int id);
 }
