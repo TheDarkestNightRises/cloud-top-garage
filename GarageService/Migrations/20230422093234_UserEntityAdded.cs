1010b217c4d0516cebd994b86c297a66b5cacac1
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-22 11:36:19 +0200

    Added migrations for user entity

---
+++ GarageService/Migrations/20230422093234_UserEntityAdded.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Garages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Garages_OwnerId",
+                table: "Garages",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Garages_Users_OwnerId",
+                table: "Garages",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Garages_Users_OwnerId",
+                table: "Garages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Garages_OwnerId",
+                table: "Garages");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Garages");
+        }
+    }
+}
