3a3e48215965881cc6b3e45bcaacecd04008aea6
TheDarkestNightRises <icecold363@gmail.com>
2023-04-22 09:39:14 +0200

    Garage service deployment fixed

--- K8s/GarageService/deploy-garage.sh
+++ K8s/GarageService/deploy-garage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Garage Service
+kubectl apply -f garage-mssql.yaml
 kubectl apply -f garage-local-pvc.yaml
 kubectl apply -f garage-mssql-initdb.yaml
 kubectl apply -f garages-mssql-depl.yaml
