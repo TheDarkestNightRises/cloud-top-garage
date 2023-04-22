22def7d749a96d3cfba0a6a0c18779e92c99ceac
TheDarkestNightRises <icecold363@gmail.com>
2023-04-22 09:41:13 +0200

    Update deploy-environment.sh

--- K8s/EnvironmentService/deploy-environment.sh
+++ K8s/EnvironmentService/deploy-environment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Environment service
+kubectl apply -f environment-mssql.yaml
 kubectl apply -f environment-local-pvc.yaml 
 kubectl apply -f environment-mssql-initdb.yaml
 kubectl apply -f environments-mssql-depl.yaml
