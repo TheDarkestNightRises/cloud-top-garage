294a589492de597b1f8aa627180e7dca6a972b76
Lichiag21 <lichiag21@gmail.com>
2023-04-24 15:35:32 +0200

    Create CarCreateDto.cs

---
+++ CarService/Dtos/CarCreateDto.cs
@@ -0,0 +1,18 @@
+namespace CarService.Dtos;
+
+using System.ComponentModel.DataAnnotations;
+using CarService.Models;
+
+public class CarCreateDto
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    // public Image Image { get; set; }
+
+    public GarageDto Garage { get; set; }
+
+}
