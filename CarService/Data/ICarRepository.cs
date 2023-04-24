b67480f7d2c8545c2785d731f25b7128c1aff6e1
Lichiag21 <lichiag21@gmail.com>
2023-04-24 15:35:29 +0200

    Update ICarRepository.cs

--- CarService/Data/ICarRepository.cs
+++ CarService/Data/ICarRepository.cs
@@ -4,12 +4,11 @@ namespace CarService.Data;
 
 public interface ICarRepository
 {
-    Task<Car> GetCarAsync(int id);
+    Task<Car?> GetCarAsync(int id);
     Task<IEnumerable<Car>> GetAllCarsAsync();
     Task<Car> AddCarAsync(Car car);
     Task<Car> UpdateCarAsync(Car car);
     Task DeleteCarAsync(int id);
-
     Task<IEnumerable<Car>> GetAllCarsAsync(CarQuery carQuery);
     Task<Image> GetCarImageAsync(int id);
 }
