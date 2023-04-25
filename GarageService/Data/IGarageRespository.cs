4ba77901dcf54a14c6a5183516b771f6f978b422
Oriana11 <rubyjpy@gmail.com>
2023-04-25 10:07:02 +0200

    Unregister garage in garage repository

--- GarageService/Data/IGarageRespository.cs
+++ GarageService/Data/IGarageRespository.cs
@@ -7,4 +7,5 @@ public interface IGarageRepository
     Task<IEnumerable<Garage>> GetAllGaragesAsync();
     Task<IEnumerable<Garage>> GetAllGaragesAsync(GarageQuery garageQuery);
     Task<Garage?> GetGarageAsync(int id);
+    Task DeleteGarageAsync(int id);
 }
