7257973e0ea7dbd17f7556439c5d501cd0534375
Oriana11 <rubyjpy@gmail.com>
2023-04-25 10:42:56 +0200

    Update GarageRepository.cs

--- GarageService/Data/GarageRepository.cs
+++ GarageService/Data/GarageRepository.cs
@@ -16,7 +16,6 @@ public class GarageRepository : IGarageRepository
     {
         // Get the garage to be deleted
         var garageToDelete = _context.Garages.Find(id);
-       
         _context.Garages.Remove(garageToDelete);
         await _context.SaveChangesAsync(); 
     }
