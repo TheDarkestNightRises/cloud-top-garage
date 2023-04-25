67a4c72dfb92d9abb106aa2fc41816587438fa5e
Lichiag21 <lichiag21@gmail.com>
2023-04-25 17:07:05 +0200

    Create CarUpdatedConsumer.cs

---
+++ GarageService/Consumers/CarUpdatedConsumer.cs
@@ -0,0 +1,37 @@
+using Contracts;
+using GarageService.Application.LogicContracts;
+using GarageService.Models;
+using MassTransit;
+
+namespace GarageService.Consumers;
+
+public class CarUpdatedConsumer : IConsumer<CarUpdated>
+{
+    private readonly ICarLogic _carLogic;
+
+    public CarUpdatedConsumer(ICarLogic carLogic)
+    {
+        _carLogic = carLogic;
+    }
+    public async Task Consume(ConsumeContext<CarUpdated> context)
+    {
+        try
+        {
+            var carCreatedMessage = context.Message.carToUpdate;
+            Car car = new Car
+            {
+                Id = carCreatedMessage.Id,
+                Name = carCreatedMessage.Name,
+                Description = carCreatedMessage.Description,
+                Image = carCreatedMessage.Image,
+                Garage = carCreatedMessage.Garage
+
+            };
+            await _carLogic.UpdateCarAsync(car);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
