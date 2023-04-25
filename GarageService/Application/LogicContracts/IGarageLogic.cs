ade0c4de7af11a25add595de0dbe046f35cec021
Oriana11 <rubyjpy@gmail.com>
2023-04-25 09:40:57 +0200

    Added definition in garage logic

--- GarageService/Application/LogicContracts/IGarageLogic.cs
+++ GarageService/Application/LogicContracts/IGarageLogic.cs
@@ -7,4 +7,6 @@ public interface IGarageLogic
     public Task<IEnumerable<Garage>> GetAllGaragesAsync();
     public Task<IEnumerable<Garage>> GetAllGaragesAsync(GarageQuery garageQuery);
     public Task<Garage?> GetGarageAsync(int id);
+
+    public Task DeleteGarageAsync(int id);
 }
