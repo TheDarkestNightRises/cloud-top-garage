0c76f7f936ab003e988aa6d35d6274ccd6c17cab
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-27 10:49:25 +0200

    Added Id to location

--- GarageService/Models/Location.cs
+++ GarageService/Models/Location.cs
@@ -2,7 +2,8 @@ namespace GarageService.Models;
 
 public class Location
 {
+    public int Id { get; set; }
     public double Latitude  { get; set; }
     public double Longitude { get; set; }
-    
+
 }
