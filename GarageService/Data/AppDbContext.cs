c4fab8c4d3a6bd8e59bc21a8d64932d12f1565d5
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-27 11:15:01 +0200

    added seed for location

--- GarageService/Data/AppDbContext.cs
+++ GarageService/Data/AppDbContext.cs
@@ -16,4 +16,6 @@ public class AppDbContext : DbContext
 
     public DbSet<User> Users { get; set; }
 
+    public DbSet<Location> Locations { get; set; }
+
 }
--- GarageService/Data/SeedDb.cs
+++ GarageService/Data/SeedDb.cs
@@ -42,15 +42,22 @@ public static class PrepDb
             var user1 = new User { };
             var user2 = new User { };
 
+            var location1 = new Location { Latitude = 55.862656, Longitude = 9.837616 };
+            var location2 = new Location { Latitude = 44.863337, Longitude = 13.857887 };
+            var location3 = new Location { Latitude = 44.420792, Longitude = 26.095582 };
+
+
             // Creating some garage instances
-            var garage1 = new Garage { Name = "Main Garage", Capacity = 5, User = user1, Cars = new List<Car> { car1, car2 } };
-            var garage2 = new Garage { Name = "Secondary", Capacity = 3, User = user1, Cars = new List<Car> { car3 } };
-            var garage3 = new Garage { Name = "Waffle", Capacity = 7, User = user2, Cars = new List<Car> { car4, car5 } };
+            var garage1 = new Garage { Name = "Main Garage", Capacity = 5, Location = location1, User = user1, Cars = new List<Car> { car1, car2 } };
+            var garage2 = new Garage { Name = "Secondary", Capacity = 3, Location = location2, Cars = new List<Car> { car3 } };
+            var garage3 = new Garage { Name = "Waffle", Capacity = 7, Location = location3, Cars = new List<Car> { car4, car5 } };
 
             // Adding the garage and car instances to the database context
             context.Cars.AddRange(new List<Car> { car1, car2, car3, car4, car5 });
             context.Users.AddRange(user1, user2);
+            context.Locations.AddRange(location1, location2, location3);
             context.Garages.AddRange(new List<Garage> { garage1, garage2, garage3 });
+            
             context.SaveChanges();
         }
         else
