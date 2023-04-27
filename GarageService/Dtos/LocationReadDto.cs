04dd4b2eae9f79aabd1a0e82bf44904f3dea9a87
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-27 10:54:47 +0200

    Create LocationReadDto.cs

---
+++ GarageService/Dtos/LocationReadDto.cs
@@ -0,0 +1,9 @@
+namespace GarageService.Dtos;
+
+public class LocationReadDto
+{
+    public int Id { get; set; }
+    public double Latitude  { get; set; }
+    public double Longitude { get; set; }
+
+}
