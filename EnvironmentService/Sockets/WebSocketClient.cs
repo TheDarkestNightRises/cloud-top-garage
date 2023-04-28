a2e716498c62782101484684c0fd20952425ebbb
TheDarkestNightRises <icecold363@gmail.com>
2023-04-28 12:48:25 +0200

    Created an example in websocketsharp

---
+++ EnvironmentService/Sockets/WebSocketClient.cs
@@ -0,0 +1,40 @@
+using System;
+using WebSocketSharp;
+namespace WebSocketSharp;
+public class WebSocketClient
+{
+    private WebSocket _webSocket;
+
+    public WebSocketClient(string url)
+    {
+        _webSocket = new WebSocket(url);
+        _webSocket.OnOpen += OnOpen;
+        _webSocket.OnMessage += OnMessage;
+        _webSocket.OnClose += OnClose;
+    }
+
+    public void Connect()
+    {
+        _webSocket.Connect();
+    }
+
+    public void Send(byte[] data)
+    {
+        _webSocket.Send(data);
+    }
+
+    private void OnOpen(object sender, EventArgs e)
+    {
+        Console.WriteLine("WebSocket opened");
+    }
+
+    private void OnMessage(object sender, MessageEventArgs e)
+    {
+        Console.WriteLine($"Received data: {BitConverter.ToString(e.RawData)}");
+    }
+
+    private void OnClose(object sender, CloseEventArgs e)
+    {
+        Console.WriteLine("WebSocket closed");
+    }
+}
