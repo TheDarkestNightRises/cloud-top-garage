a159d3a64c45f091d74a0345acbbe608eb161d76
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-30 23:24:19 +0200

    create garage method for garage logic (car service)

--- CarService/Application/LogicContracts/IGarageLogic.cs
+++ CarService/Application/LogicContracts/IGarageLogic.cs
@@ -4,6 +4,7 @@ namespace CarService.Application.LogicContracts;
 
 public interface IGarageLogic
 {
+    public Task CreateGarageAsync(int garageId);
     public Task DeleteGarageAsync(int id);
 
     public Task<Garage?> GetGarageAsync(int id);
