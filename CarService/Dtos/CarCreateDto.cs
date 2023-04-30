d3bc574b9858f13efd0dc5c6c605df660213919b
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-04-30 21:06:02 +0200

    added missing fields to CarCreateDto

--- CarService/Dtos/CarCreateDto.cs
+++ CarService/Dtos/CarCreateDto.cs
@@ -7,10 +7,17 @@ public class CarCreateDto
 {
     [Required]
     public string Name { get; set; }
-
     [Required]
     public string Description { get; set; }
-
+    [Required]
+    public string Manufacturer { get; set; }
+    [Required]
+    public string Model { get; set; }
+    [Required]
+    public int Year { get; set; }
+    [Required]
+    public int Seats { get; set; }
     [Required]
     public GarageDto Garage { get; set; }
+    public EngineCreateDto Engine { get; set; }
 }
