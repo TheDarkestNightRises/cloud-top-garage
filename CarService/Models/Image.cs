b7dff5fc1c0a61e978f2c1c7734e473b0a9b3a8a
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-05-11 02:31:27 +0200

    added equals method in Image class

--- CarService/Models/Image.cs
+++ CarService/Models/Image.cs
@@ -4,4 +4,45 @@ public class Image
 {
     public int Id { get; set; }
     public byte[] Data { get; set; }
+    public override bool Equals(object obj)
+    {
+        // If the object is null, return false
+        if (obj == null)
+        {
+            return false;
+        }
+
+        // If the object cannot be cast to an Image, return false
+        if (!(obj is Image))
+        {
+            return false;
+        }
+
+        // Cast the object to an Image
+        Image other = (Image)obj;
+
+        // Compare the Id property
+        if (Id != other.Id)
+        {
+            return false;
+        }
+
+        // Compare the Data property
+        if (Data == null && other.Data != null)
+        {
+            return false;
+        }
+        else if (Data != null && other.Data == null)
+        {
+            return false;
+        }
+        else if (Data != null && other.Data != null && !Data.SequenceEqual(other.Data))
+        {
+            return false;
+        }
+
+        // If we've made it this far, the objects are equal
+        return true;
+    }
+
 }
