fb810d972f6de5b70aecc971e170cc3898d1d37e
Oriana11 <rubyjpy@gmail.com>
2023-05-12 11:10:48 +0200

    The socket can translate data now

--- EnvironmentService/Sockets/WebSocketClient.cs
+++ EnvironmentService/Sockets/WebSocketClient.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Text;
+using EnvironmentService.Application.LogicContracts;
+using Newtonsoft.Json.Linq;
 using WebSocketSharp;
 namespace WebSocketSharp;
 public class WebSocketClient
@@ -16,7 +19,7 @@ public class WebSocketClient
 
     public void Connect()
     {
-        Console.WriteLine("WebSocket connecting");
+        Console.WriteLine("--> WebSocket connecting");
         try
         {
             _webSocket.Connect();
@@ -25,7 +28,7 @@ public class WebSocketClient
         {
             Console.WriteLine(e.Message);
         }
-        Console.WriteLine("WebSocket connected");
+        Console.WriteLine("--> WebSocket connected");
     }
 
     public void Send(byte[] data)
@@ -35,16 +38,58 @@ public class WebSocketClient
 
     private void OnOpen(object sender, EventArgs e)
     {
-        Console.WriteLine("WebSocket opened");
+        Console.WriteLine("--> WebSocket opened");
     }
 
     private void OnMessage(object sender, MessageEventArgs e)
     {
-        Console.WriteLine($"Received data: {BitConverter.ToString(e.RawData)}");
+        byte[] byteArray = e.RawData; // Assuming e.RawData is your byte array
+        string byteArrayString = BitConverter.ToString(byteArray);
+        string[] hexValues = byteArrayString.Split('-');
+        byte[] newByteArray = new byte[hexValues.Length];
+
+        for (int i = 0; i < hexValues.Length; i++)
+        {
+            newByteArray[i] = byte.Parse(hexValues[i], System.Globalization.NumberStyles.HexNumber);
+        }
+
+        string result = Encoding.UTF8.GetString(newByteArray);
+        var jsonObject = JObject.Parse(result);
+        var dataValue = (string)jsonObject["data"];
+        Console.WriteLine($"data: {dataValue}");
+        TranslateData(dataValue);
     }
 
     private void OnClose(object sender, CloseEventArgs e)
     {
-        Console.WriteLine("WebSocket closed");
+        Console.WriteLine("--> WebSocket closed");
+    }
+
+
+    public void TranslateData(string dataValue)
+    {
+        // Extract the individual components from the hex string
+        string c02Hex = dataValue.Substring(0, 4);
+        string temperatureHex = dataValue.Substring(4, 4);
+        string humidityHex = dataValue.Substring(8, 4);
+        string lightHex = dataValue.Substring(12, 4); //Luminisoty per square meter
+        string motionAlarmHex = dataValue.Substring(16, 2);
+        string idMacAddressHex = dataValue.Substring(18, 2);
+
+        // Convert hex strings to decimal values
+        int c02 = Convert.ToInt32(c02Hex, 16);
+        int temperature = Convert.ToInt32(temperatureHex, 16);
+        int humidity = Convert.ToInt32(humidityHex, 16);
+        int light = Convert.ToInt32(lightHex, 16);
+        int motionAlarm = Convert.ToInt32(motionAlarmHex, 16);
+        int idMacAddress = Convert.ToInt32(idMacAddressHex, 16);
+
+        // Print the translated values
+        Console.WriteLine($"CO2: {c02}");
+        Console.WriteLine($"Temperature: {temperature}");
+        Console.WriteLine($"Humidity: {humidity}");
+        Console.WriteLine($"Light: {light}");
+        Console.WriteLine($"Motion/Alarm: {motionAlarm}");
+        Console.WriteLine($"ID/MAC Address: {idMacAddress}");
     }
 }
