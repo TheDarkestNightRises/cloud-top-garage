64253b44ebae4514f6bbc054d874de44e8354435
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2023-05-18 11:39:58 +0200

    Created Data layer

--- EnvironmentService/Data/IStatRepository.cs
+++ EnvironmentService/Data/IStatRepository.cs
@@ -5,4 +5,5 @@ public interface IStatRepository
 {
     Task<Stat> AddStatAsync(Stat stat);
     Task<IEnumerable<Stat>> GetAllStatsAsync();
+    Task<IEnumerable<Stat>> GetAllStatsAsync(StatQuery statQuery);
 }
