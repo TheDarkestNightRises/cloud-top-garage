49efd854535bf74d513430d855fdc43b8d975044
Lichiag21 <lichiag21@gmail.com>
2023-05-20 14:56:59 +0300

    garage testing done

--- GarageService/Tests/GarageRepositoryTests.cs
+++ GarageService/Tests/GarageRepositoryTests.cs
@@ -129,4 +129,126 @@ public class GarageRepositoryTests
         Assert.NotEmpty(result);
         Assert.Equal(2, result.Count());
     }
+
+    [Fact]
+    public async Task GetAllGaragesAsync_ReturnsEmptyList_WhenInvalidUserId()
+    {
+        // Arrange
+        var context = new AppDbContext(_dbContextOptions);
+        var repository = new GarageRepository(context);
+
+        var user1 = new User { Id = 1 };
+        var location1 = new Location { Latitude = 55.862656, Longitude = 9.837616 };
+        var garage1 = new Garage { Name = "Garage 1", Capacity = 5, Location = location1, User = user1 };
+        context.Garages.Add(garage1);
+        await context.SaveChangesAsync();
+
+        var garageQuery = new GarageQuery { UserId = -1 };
+        // Act
+        var result = await repository.GetAllGaragesAsync(garageQuery);
+
+        // Assert
+        Assert.Empty(result);
+    }
+
+    // ----------------------- UPDATE GARAGE --------------------------------------------------
+
+    [Fact]
+    public async Task UpdateGarageAsync_OneField_UpdatesGarageInDatabase()
+    {
+        // Arrange
+        var context = new AppDbContext(_dbContextOptions);
+        var repository = new GarageRepository(context);
+
+        var car1 = new Car { };
+        var car2 = new Car { };
+        var user1 = new User { };
+        var location1 = new Location { Latitude = 55.862656, Longitude = 9.837616 };
+        var garage = new Garage { Name = "Garage", Capacity = 5, Location = location1, User = user1, Cars = new List<Car> { car1, car2 } };
+        context.Garages.Add(garage);
+
+        await context.SaveChangesAsync();
+
+        // Modify the garage
+        garage.Name = "Updated Garage";
+
+        // Act
+        await repository.UpdateGarageAsync(garage);
+
+        // Retrieve the garage from the database
+        var updatedGarage = await context.Garages.FindAsync(1);
+
+        // Assert
+        Assert.Equal(garage.Name, updatedGarage?.Name);
+    }
+
+    [Fact]
+    public async Task UpdateGarageAsync_TwoFields_UpdatesGarageInDatabase()
+    {
+        // Arrange
+        var context = new AppDbContext(_dbContextOptions);
+        var repository = new GarageRepository(context);
+
+        var car1 = new Car { };
+        var car2 = new Car { };
+        var user1 = new User { };
+        var location1 = new Location { Latitude = 55.862656, Longitude = 9.837616 };
+        var garage = new Garage { Name = "Garage", Capacity = 5, Location = location1, User = user1, Cars = new List<Car> { car1, car2 } };
+        context.Garages.Add(garage);
+
+        await context.SaveChangesAsync();
+
+        // Modify the garage
+        garage.Name = "Updated Garage";
+        garage.Capacity = 6;
+
+        // Act
+        await repository.UpdateGarageAsync(garage);
+
+        // Retrieve the garage from the database
+        var updatedGarage = await context.Garages.FindAsync(1);
+
+        // Assert
+        Assert.Equal(garage.Name, updatedGarage?.Name);
+        Assert.Equal(garage.Capacity, updatedGarage?.Capacity);
+    }
+
+    // ----------------------- DELETE GARAGE --------------------------------------------------
+
+    [Fact]
+    public async Task DeleteGarageAsync_DeletesExistingGarage()
+    {
+        // Arrange
+        var context = new AppDbContext(_dbContextOptions);
+        var repository = new GarageRepository(context);
+        int garageId = 1;
+        var car1 = new Car { };
+        var car2 = new Car { };
+        var user1 = new User { };
+        var location1 = new Location { Latitude = 55.862656, Longitude = 9.837616 };
+        var garage = new Garage { Name = "Garage", Capacity = 5, Location = location1, User = user1, Cars = new List<Car> { car1, car2 } };
+        context.Garages.Add(garage);
+
+        await context.SaveChangesAsync();
+
+        // Act
+        await repository.DeleteGarageAsync(garageId);
+
+        // Assert
+        var deletedGarage = await context.Garages.FindAsync(garage.Id);
+        Assert.Null(deletedGarage);
+    }
+
+    [Fact]
+    public async Task DeleteGarageAsync_WhenGarageDoesNotExist()
+    {
+        // Arrange
+        var context = new AppDbContext(_dbContextOptions);
+        var repository = new GarageRepository(context);
+
+        var nonExistentGarageId = 1;
+
+        // Act and Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(async () => await repository.DeleteGarageAsync(nonExistentGarageId));
+    }
 }
