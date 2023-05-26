9a3c86c76274f90f0fffcf03e1d13cf56c1f6fb0
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2023-05-26 04:02:03 +0200

    Update IStatRepository.cs

--- EnvironmentService/Data/IStatRepository.cs
+++ EnvironmentService/Data/IStatRepository.cs
@@ -6,4 +6,5 @@ public interface IStatRepository
     Task<Stat> AddStatAsync(Stat stat);
     Task<IEnumerable<Stat>> GetAllStatsAsync();
     Task<IEnumerable<Stat>> GetAllStatsAsync(StatQuery statQuery);
+    Task<Stat?> GetLastestStatAsync(StatQuery statQuery);
 }
